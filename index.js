73d7c1593444649ff763d9bfed93987ddfdb3965
Triston <tristonherbst97@gmail.com>
2021-04-04 22:18:01 -0500

    commit tree 73e0956c66a951a73c7b3268c5ef994a231135e4

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+
+let drivers = ['Milo', 'Otis', 'Garfield'];
+
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+}
+
+function appendDriver(name) {
+    new_array = [...drivers,name];
+}
+
+function prependDriver(name) {
+    new_array = [name,...drivers];
+}
+
+function removeLastDriver() {
+    array = drivers.slice(-1);
+}
+
+function removeFirstDriver( ){
+    array = drivers.slice(0);
+}
